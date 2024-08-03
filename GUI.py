298efd913b70c242dbbd8fde96006018e1621b0f
Grten3 <andy04.uni@gmail.com>
2024-08-03 18:00:50 +1200

    GUI

--- GUI.py
+++ GUI.py
@@ -1,3 +1,89 @@
+from tkinter import *
+from PIL import Image, ImageTk
+import os
 
 
+# functions
+def get_image(frame, bg, filepath, resize):
+    """returns a working image label"""
+    IMG = Image.open(filepath)
+    image = ImageTk.PhotoImage(IMG.resize(resize))
+    img_lbl = Label(frame, image=image, bg=bg)
+    img_lbl.image = image
+    return img_lbl
 
+
+# main GUI class
+class GUI:
+    """this class runs the interface"""
+    def __init__(self, parent):
+        self.parent = parent
+        self.MED_FONT = ("Helvetica", 65, "bold")
+        self.MED_FONT1 = ("Helvetica", 36)
+        self.MED_FONT2 = ("Helvetica", 36, "bold")
+        self.SMALL_FONT = ("Helvetica", 26, "bold")
+        self.SMALL_FONT2 = ("Helvetica", 15)
+        self.SMALL_FONT3 = ("Helvetica", 18)
+        self.SMALL_FONT4 = ("Helvetica", 15, "underline")
+        self.SMALL_FONT5 = ("Helvetica", 15, "bold")
+        self.TINY_FONT = ("Helvetica", 12)
+        self.TINY_FONT2 = ("Helvetica", 12, "underline")
+
+        self.screen = ""
+        self.current_screen = ""
+        self.current_stock_cards = []
+
+        # Initialize the full-screen black background
+        self.main = Frame(parent, bg='black')
+        self.main.pack(fill=BOTH, expand=True)
+
+       
+
+        self.on = Frame(self.main, bg='gray')
+        self.on.pack(fill=BOTH, expand=True)
+
+        # Add the "Click Anywhere to Start" label
+        self.start_prompt = Label(self.on, text="Click Anywhere to Start", fg="white", bg="black", font=self.MED_FONT1)
+        self.start_prompt.pack(expand=True)
+
+      
+
+        # Bind the click event to start the main interface
+        self.on.bind("<Button-1>", self.start_interface)
+        self.start_prompt.bind("<Button-1>", self.start_interface)
+
+    def start_interface(self, event=None):
+        # Remove the label
+        self.forget_screen(self.on)
+        bg = get_image(self.main, 'white', 'face_pics/Annoy_Squidward.jpg', (850, 850))
+        bg.pack(expand=True,anchor=S)
+
+    @staticmethod
+    def forget_screen(widgets):
+        """forgets all the widgets on the window given the list of widgets"""
+        try:
+            for wid in widgets:
+                wid.pack_forget()
+        except:
+            widgets.pack_forget()
+
+    @staticmethod
+    def destroy_screen(widgets):
+        """destroys all the widgets on the window given the list of widgets"""
+        try:
+            for wid in widgets:
+                wid.destroy()
+        except:
+            widgets.destroy()
+
+    def face(self):
+        pass
+
+
+# mainloop
+root = Tk()
+GUI(root)
+root.title("SquirtWard Face")
+root.attributes("-fullscreen", True)  # Make the window full-screen
+root.bind("<Escape>", lambda e: root.destroy())  # Pressing Escape will exit the full-screen mode
+root.mainloop()
--- data.py
+++ data.py
@@ -5,72 +5,69 @@ import speech_recognition as sr
 import warnings
 from playsound import playsound
 
+class Response:
 
-
-def mic_check(r, microphone): 
-    # When click to start, start recognising microphone
-    with microphone as source: 
-        print("Listening...")
-        r.adjust_for_ambient_noise(source)
-        audio = r.listen(source, timeout=15, phrase_time_limit=15)
-  
-    try:
-        # Recognize speech using Google Web Speech API
-        response = r.recognize_google(audio)
-    except sr.RequestError:
-        # API was unreachable or unresponsive
-        response = "API unavailable"
-    except sr.UnknownValueError:
-        # Speech was unintelligible
-        response = "Unable to recognize speech"
-
-    print(response + "\n")
-    check_flirting(response)
-
-
-def check_flirting(text):
-    with open("key.txt") as key_file:
-        key = key_file.read()
+    def speech_to_text(self): 
+        # When click to start, start recognising microphone
+        r = sr.Recognizer()
+        m = sr.Microphone()
+        with m as source: 
+            print("Listening...")
+            r.adjust_for_ambient_noise(source)
+            audio = r.listen(source, timeout=15, phrase_time_limit=15)
     
-    client = OpenAI(api_key = key)
+        try:
+            # Recognize speech using Google Web Speech API
+            response = r.recognize_google(audio)
+        except sr.RequestError:
+            # API was unreachable or unresponsive
+            response = "API unavailable"
+        except sr.UnknownValueError:
+            # Speech was unintelligible
+            response = "Unable to recognize speech"
 
-    completion = client.chat.completions.create(
-      model="gpt-4o-mini",
-      messages=[
-        {"role": "system", "content": "You are handsome Squidward from Spongebob. You are feeling humourous and edgy today, and someone is flirting with you. Respond with a 1 if it's good and 0 otherwise, followed by a space and short message of why you think the flirting is good or not"},
-        {"role": "user", "content": text}
-      ]
-    )
-    bin_ans = completion.choices[0].message.content[0]
-    txt_ans = completion.choices[0].message.content[1:]
-    print(txt_ans + "\n")
-    tts(txt_ans)
-
-
-def tts(txt_ans):
-    # Ignore DeprecationWarning
-    warnings.filterwarnings("ignore", category=DeprecationWarning)
+        print(response + "\n")
+        self.__check_flirting(response) 
 
 
-    with open("key.txt") as key_file:
+    def __check_flirting(self,text):
+        with open("key.txt") as key_file:
             key = key_file.read()
         
-    client = OpenAI(api_key = key)
+        client = OpenAI(api_key = key)
 
-    response = client.audio.speech.create(
-    model="tts-1",
-    voice="alloy",
-    input=txt_ans
-    )
+        completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You are handsome Squidward from Spongebob. You are feeling humourous and edgy today, and someone is flirting with you. Respond with a 1 if it's good and 0 otherwise, followed by a space and short message (spoken like squidward) of why you think the flirting is good or not"},
+            {"role": "user", "content": text}
+        ]
+        )
+        bin_ans = completion.choices[0].message.content[0]
+        txt_ans = completion.choices[0].message.content[1:]
+        print(txt_ans + "\n")
+        self.__text_to_speech(txt_ans)
 
-    response.stream_to_file("speech.mp3")
-    playsound("speech.mp3")
+
+    def __text_to_speech(self,txt_ans):
+        # Ignore DeprecationWarning
+        warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+
+        with open("key.txt") as key_file:
+                key = key_file.read()
+            
+        client = OpenAI(api_key = key)
+
+        response = client.audio.speech.create(
+        model="tts-1",
+        voice="alloy",
+        input=txt_ans
+        )
+
+        response.stream_to_file("speech.mp3")
+        playsound("speech.mp3")
 
 
 
-if __name__ == '__main__': 
-    r = sr.Recognizer()
-    m = sr.Microphone()
-    mic_check(r, m)
- 
 
