ad4f9866f7ed4c4ca720d47921e3b25cb423409b
Grten3 <andy04.uni@gmail.com>
2024-08-03 15:41:01 +1200

    GUI

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from openai import OpenAI
 import speech_recognition as sr 
 #  Run pip install pyAudio 
 # pip install SpeechRecognition
+import warnings
+from playsound import playsound
+
+
 
 def mic_check(r, microphone): 
     # When click to start, start recognising microphone
@@ -33,12 +37,37 @@ def check_flirting(text):
     completion = client.chat.completions.create(
       model="gpt-4o-mini",
       messages=[
-        {"role": "system", "content": "You are handsome Squidward from Spongebob. You are feeling humourous and edgy today, and someone is flirting with you. Respond with a short message of why you think the flirting is goo dor not"},
+        {"role": "system", "content": "You are handsome Squidward from Spongebob. You are feeling humourous and edgy today, and someone is flirting with you. Respond with a 1 if it's good and 0 otherwise, followed by a space and short message of why you think the flirting is good or not"},
         {"role": "user", "content": text}
       ]
     )
-    answer = completion.choices[0].message.content
-    print(answer)
+    bin_ans = completion.choices[0].message.content[0]
+    txt_ans = completion.choices[0].message.content[1:]
+    print(txt_ans + "\n")
+    tts(txt_ans)
+
+
+def tts(txt_ans):
+    # Ignore DeprecationWarning
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+
+    with open("key.txt") as key_file:
+            key = key_file.read()
+        
+    client = OpenAI(api_key = key)
+
+    response = client.audio.speech.create(
+    model="tts-1",
+    voice="alloy",
+    input=txt_ans
+    )
+
+    response.stream_to_file("speech.mp3")
+    playsound("speech.mp3")
+
+
+
 
 
 if __name__ == '__main__': 
